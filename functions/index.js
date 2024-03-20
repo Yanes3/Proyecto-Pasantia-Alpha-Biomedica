00a52a3e3c5aa48686c89102cffd09079e6c8877
Yanes3 <cye13766@hotmail.com>
2024-03-19 23:34:39 -0500

    Correciones pero sin conectar a base

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,38 @@
-const functions = require('firebase-admin');
-const admin = requiere('firebase-admin');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+const admin = requiere("firebase-admin");
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+
 
 admin.initializeApp();
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-   response.send("Hola Tonotos");
- });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello Tonotos");
+});
 
- exports.getScreams = functions.https.onRequest((req,res) =>{
-  admin.firestore().collection('screams').get()
-  .then(data =>{
-    let screams = [];
-    data.forEach(doc => {
-      screams.push(doc.data());
-    });
-    return res.json(screams);
-  })
-  .catch((err) => console.error(err));
- })
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
