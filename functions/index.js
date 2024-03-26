1c233ae9da1587f91203d81953ef27271528e620
Yanes3 <cye13766@hotmail.com>
2024-03-25 20:56:50 -0500

    Se crea funcion para agregar Scream

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,51 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-   response.send("Hola Tonotos");
- });
-
-exports.getScreams = functions.https.onRequest((req,res) =>{
-  admin.firestore().collection('screams').get()
-  .then(data =>{
-    let screams = [];
-    data.forEach(doc => {
-      screams.push(doc.data());
-    });
-    return res.json(screams);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send(err); // También envía un error al cliente
-  });
+  response.send("Hola Tonotos");
 });
 
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+    if(req.method !== 'POST'){
+        return res.status(400).json({error: 'Method not allowed'});
+    }
+    const newScream = {
+      body: req.body.body,
+      userHandle: req.body.userHandle,
+      createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    };
+    admin
+      .firestore()
+      .collection("screams")
+      .add(newScream)
+      .then((doc) => {
+        res.json({ message: `document ${doc.id} created successfully` }); 
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "something went wrong" });
+        console.error(err);
+      });
+  });
+  
