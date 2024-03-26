8c2924b11a27fee21069559a83bf660773bac508
Yanes3 <cye13766@hotmail.com>
2024-03-25 19:20:31 -0500

    Primera conexion de BD correcta

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,26 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-const admin = requiere("firebase-admin");
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const functions = require("firebase-functions");
-
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello Tonotos");
+exports.helloWorld = functions.https.onRequest((request, response) => {
+   response.send("Hola Tonotos");
+ });
+
+exports.getScreams = functions.https.onRequest((req,res) =>{
+  admin.firestore().collection('screams').get()
+  .then(data =>{
+    let screams = [];
+    data.forEach(doc => {
+      screams.push(doc.data());
+    });
+    return res.json(screams);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send(err); // También envía un error al cliente
+  });
 });
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
-    .get()
-    .then((data) => {
-      let screams = [];
-      data.forEach((doc) => {
-        screams.push(doc.data());
-      });
-      return res.json(screams);
-    })
-    .catch((err) => console.error(err));
-});
