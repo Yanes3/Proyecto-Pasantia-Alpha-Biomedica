d934d75121417c82379a2bb24904adfe0947ad0c
Yanes3 <cye13766@hotmail.com>
2024-03-25 21:35:57 -0500

    Inicio de express

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,12 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+const express = require("express");
+const app = express();
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
+
+app.get("/screams", (req, res));
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hola Tonotos");
@@ -28,24 +32,23 @@ exports.getScreams = functions.https.onRequest((req, res) => {
 });
 
 exports.createScream = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed'});
-    }
-    const newScream = {
-      body: req.body.body,
-      userHandle: req.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-    };
-    admin
-      .firestore()
-      .collection("screams")
-      .add(newScream)
-      .then((doc) => {
-        res.json({ message: `document ${doc.id} created successfully` }); 
-      })
-      .catch((err) => {
-        res.status(500).json({ error: "something went wrong" });
-        console.error(err);
-      });
-  });
-  
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
+  }
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
