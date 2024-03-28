1e3708cb139e9bc1fbcfc4ecd710f419079f869c
Yanes3 <cye13766@hotmail.com>
2024-03-28 13:15:46 -0500

    Creacion de usuarios completa

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams:[
+        {
+            userHandle: 'user',
+            body:'This is the scream body',
+            creadtedAt: '2024-03-28T18:10:10.847Z',
+            likeCount: 5,
+            commmetCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -4,24 +4,22 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const express = require("express");
 const app = express();
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-app.get("/screams", (req, res));
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hola Tonotos");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
+    .orderBy('createdAt','desc')
     .get()
     .then((data) => {
       let screams = [];
       data.forEach((doc) => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(screams);
     })
@@ -31,15 +29,13 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: "Method not allowed" });
-  }
+app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString()
   };
+
   admin
     .firestore()
     .collection("screams")
@@ -52,3 +48,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.api = functions.https.onRequest(app);
