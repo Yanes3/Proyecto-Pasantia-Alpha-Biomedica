9452a2fc8b62723e5171fd5286d0fa08568f4ccd
Yanes3 <cye13766@hotmail.com>
2024-03-29 13:48:12 -0500

    Creacion de usuarios

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
 
 admin.initializeApp();
-const express = require("express");
-const app = express();
+
+const config = {
+  apiKey: "AIzaSyBNeUkPw4SGui__gd1nNjWIgVxB9074_b8",
+  authDomain: "proyecto-pasantia-biomedica.firebaseapp.com",
+  databaseURL:
+    "https://proyecto-pasantia-biomedica-default-rtdb.firebaseio.com",
+  projectId: "proyecto-pasantia-biomedica",
+  storageBucket: "proyecto-pasantia-biomedica.appspot.com",
+  messagingSenderId: "788286258188",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
 
 app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
-    .orderBy('createdAt','desc')
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let screams = [];
@@ -33,7 +45,7 @@ app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString()
+    createdAt: new Date().toISOString(),
   };
 
   admin
@@ -46,6 +58,28 @@ app.post("/scream", (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: "something went wrong" });
       console.error(err);
+    });
+});
+
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+  
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
 });
 
