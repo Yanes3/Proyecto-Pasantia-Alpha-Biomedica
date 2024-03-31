e32f023413181eb11f7f3e32f7a65de8df980fbc
Yanes3 <cye13766@hotmail.com>
2024-03-31 16:28:08 -0500

    Creacion de comentarios y respuesta de los mismos

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,12 @@ const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
 
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOneScream,
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -15,11 +20,14 @@ const {
 
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
-app.post("/user/image", FBAuth, uploadImage);
-app.post("/user", FBAuth, addUserDetails);
-app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/scream/:screamId", getScream);
+
+app.post("/scream/:screamId/comment", FBAuth, commentOneScream);
 
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
