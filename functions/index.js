c489fe3237164d453e8f2398a29d156085fb6cef
Yanes3 <cye13766@hotmail.com>
2024-03-31 17:19:52 -0500

    Todo el CRUD de likes y comentarios completo

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,9 @@ const {
   postOneScream,
   getScream,
   commentOneScream,
+  likeScream,
+  unlikeScream,
+  deleteScream
 } = require("./handlers/screams");
 const {
   signup,
@@ -21,7 +24,9 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOneScream);
 
 app.post("/signup", signup);
