8849ed304b391f927dc9d08276bb0bdff4ff9a93
Yanes3 <cye13766@hotmail.com>
2024-03-31 12:10:15 -0500

    Funcion genera imagen al crear la cuenta p[ero no permite cambiar la misma

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,11 +1,11 @@
-const isEmpty = (string) => {
-  if (string.trim() === "") return true;
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
   else return false;
 };
 
-const isEmail = (email) => {
-  const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (email.match(regEx)) return true;
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
   else return false;
 };
 
