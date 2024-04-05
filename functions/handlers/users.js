46813a738824f0936292b75f1b049978b4b48ef5
Yanes3 <cye13766@hotmail.com>
2024-04-05 15:39:55 -0500

    TypeError: BusBoy is not a constructor (Corregido)

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -6,7 +6,11 @@ const firebase = require("firebase");
 
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData , reduceUserDetails } = require("../util/validators");
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails,
+} = require("../util/validators");
 
 exports.signup = (req, res) => {
   const newUser = {
@@ -45,7 +49,7 @@ exports.signup = (req, res) => {
         email: newUser.email,
         createdAt: new Date().toISOString(),
         imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
-        userId
+        userId,
       };
       return db.doc(`/users/${newUser.handle}`).set(userCredentials);
     })
@@ -57,7 +61,9 @@ exports.signup = (req, res) => {
       if (err.code === "auth/email-already-in-use") {
         return res.status(400).json({ email: "Email is already in use" });
       } else {
-        return res.status(500).json({ general: "Something went wrong, please try again" });
+        return res
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
       }
     });
 };
@@ -83,95 +89,108 @@ exports.login = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-        return res
-          .status(403)
-          .json({ general: "Wrong credentials, please try again" });
+      return res
+        .status(403)
+        .json({ general: "Wrong credentials, please try again" });
     });
 };
 
 exports.addUserDetails = (req, res) => {
   let userDetails = reduceUserDetails(req.body);
 
-  db.doc(`/users/${req.user.handle}`).update(userDetails)
-  .then(() => {
-    return res.json({ message: "Details added successfully" });
-  })
-  .catch((err) => {
-    console.error(err);
-    return res.status(500).json({ error: err.code });
-  });
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: "Details added successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 };
 
 exports.getUserDetails = (req, res) => {
   let userData = {};
-  db.doc(`/users/${req.params.handle}`).get()
-  .then(doc => {
-    if(doc.exists){
-      userData.user = doc.data();
-      return db.collection('screams').where('userHandle', '==', req.params.handle).orderBy('createdAt', 'desc').get();
-    } else {
-      return res.status(404).json({ error: 'User not found' });
-    }
-  })
-  .then(data => {
-    userData.screams = [];
-    data.forEach(doc => {
-      userData.screams.push({
-        body: doc.data().body,
-        createdAt: doc.data().createdAt,
-        userHandle: doc.data().userHandle,
-        userImage: doc.data().userImage,
-        likeCount: doc.data().likeCount,
-        commentCount: doc.data().commentCount,
-        screamId: doc.id
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection("screams")
+          .where("userHandle", "==", req.params.handle)
+          .orderBy("createdAt", "desc")
+          .get();
+      } else {
+        return res.status(404).json({ error: "User not found" });
+      }
+    })
+    .then((data) => {
+      userData.screams = [];
+      data.forEach((doc) => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id,
+        });
       });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
-    return res.json(userData);
-  })
-  .catch(err => {
-    console.error(err);
-    return res.status(500).json({ error: err.code });
-  });
-}
-
-
+};
 
 exports.getAuthenticatedUser = (req, res) => {
   let userData = {};
-  db.doc(`/users/${req.user.handle}`).get()
-  .then((doc) => {
-    if(doc.exists){
-      userData.credentials = doc.data();
-      return db.collection('likes').where('userHandle', '==', req.user.handle).get();
-    }
-  })
-  .then(data => {
-    userData.likes = [];
-    data.forEach(doc => {
-      userData.likes.push(doc.data());
-    });
-    return db.collection('notifications').where('recipient', '==', req.user.handle).orderBy('createdAt', 'desc').limit(10).get();
-  })
-  .then(data => {
-    userData.notifications = [];
-    data.forEach(doc => {
-      userData.notifications.push({
-        recipient: doc.data().recipient,
-        sender: doc.data().sender,
-        createdAt: doc.data().createdAt,
-        screamId: doc.data().screamId,
-        type: doc.data().type,
-        read: doc.data().read,
-        notificationId: doc.id
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then((data) => {
+      userData.likes = [];
+      data.forEach((doc) => {
+        userData.likes.push(doc.data());
       });
+      return db
+        .collection("notifications")
+        .where("recipient", "==", req.user.handle)
+        .orderBy("createdAt", "desc")
+        .limit(10)
+        .get();
+    })
+    .then((data) => {
+      userData.notifications = [];
+      data.forEach((doc) => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
-    return res.json(userData);
-  })
-  .catch(err => {
-    console.error(err);
-    return res.status(500).json({ error: err.code });
-  });
-}
+};
 
 exports.uploadImage = (req, res) => {
   const BusBoy = require("busboy");
@@ -179,30 +198,42 @@ exports.uploadImage = (req, res) => {
   const os = require("os");
   const fs = require("fs");
 
-  const busboy = new BusBoy({ headers: req.headers });
+  const busboy = BusBoy({ headers: req.headers });
 
-  let imageFileName;
   let imageToBeUploaded = {};
+  let imageFileName;
 
-  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    /*/
-    TypeError: BusBoy is not a constructor
-    console.log(fieldname);
-    console.log(filename);
-    console.log(mimetype);/*/
+  busboy.on("file", (fieldname, file, info) => {
+    console.log(
+      "FieldName Fuck:" + fieldname,
+      "File Fuck:" + file,
+      "Filename Fuck:" + info.filename,
+      "Encoding Fuck:" + info.encoding,
+      "Mimetype Fuck:" + info.mimeType
+    );
 
-    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+    if (info.mimeType !== "image/jpeg" && info.mimeType !== "image/png") {
+      console.log("Entré a Validación");
+      console.log("Mimetype Validación" + info.mimeType);
       return res.status(400).json({ error: "Wrong file type submitted" });
     }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension =
+      info.filename.split(".")[info.filename.split(".").length - 1];
 
-    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    // 32756238461724837.png
     imageFileName = `${Math.round(
       Math.random() * 1000000000000
-    )}.${imageExtension}`;
+    ).toString()}.${imageExtension}`;
+
     const filepath = path.join(os.tmpdir(), imageFileName);
-    imageToBeUploaded = { filepath, mimetype };
+
+    const filemimetype = info.mimeType;
+
+    imageToBeUploaded = { filepath, filemimetype };
     file.pipe(fs.createWriteStream(filepath));
-  }); 
+  });
+
   busboy.on("finish", () => {
     admin
       .storage()
@@ -216,6 +247,7 @@ exports.uploadImage = (req, res) => {
         },
       })
       .then(() => {
+        // Append token to url
         const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
         return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
       })
@@ -224,7 +256,7 @@ exports.uploadImage = (req, res) => {
       })
       .catch((err) => {
         console.error(err);
-        return res.status(500).json({ error: err.code });
+        return res.status(500).json({ error: "something went wrong" });
       });
   });
   busboy.end(req.rawBody);
@@ -232,16 +264,17 @@ exports.uploadImage = (req, res) => {
 
 exports.markNotificationsRead = (req, res) => {
   let batch = db.batch();
-  req.body.forEach(notificationId => {
+  req.body.forEach((notificationId) => {
     const notification = db.doc(`/notifications/${notificationId}`);
     batch.update(notification, { read: true });
   });
-  batch.commit()
-  .then(() => {
-    return res.json({ message: 'Notifications marked read' });
-  })
-  .catch(err => {
-    console.error(err);
-    return res.status(500).json({ error: err.code });
-  });
-}
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: "Notifications marked read" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
